68fe6c04735bca149d20603f26577710e9b2e912
Jonathan Lamar <jon.lamar@ironcorelabs.com>
2018-02-12 20:06:48 -0700

    saving progress

--- neural_net.py
+++ neural_net.py
@@ -21,51 +21,43 @@ class neural_net(object):
 	def set_penalty(self, val):
 		self.penalty = val
 
-	def theta(self, l):
+	def theta(self, l, weights = self.weights):
 		N = sum([self.nodes[i+1] * (self.nodes[i] + 1) for i in range(l-1)])
 		return np.matrix(np.reshape(self.weights[N:N + self.nodes[l]*(self.nodes[l-1]+1)], (self.nodes[l], self.nodes[l-1]+1)))
 
-	def debug_cost(self, X, y):
-		# TODO: Remove if this matches self.cost.
+	def cost(self, X, y, weights = self.weights):
 		# y has shape m by K
 		# h has shape m by K
 		L = len(self.nodes) # number of layers (including input and output layers)
 		K = self.nodes[-1] # dimension of output layer (number of classes)
 		m = X.shape[0] # number of training examples
 		h = self.h(X) # output matrix
-		logh = np.log(h)
-		one_minus_y = np.ones(y.shape) - y
-		log_one_minus_h = np.log(np.ones(h.shape) - h)
 
 		acc = 0
 		for i in range(1, m+1):
-			for k in range(1, K+1):
-				acc -= (y[i,k]*logh[i,k] + one_minus_y[i,k]*log_one_minus_h[i,k])
+			acc -= (y[i].transpose()*np.log(h[i]) + (1-y[i]).transpose()*np.log(1-h[i]))
 
 		acc2 = 0
-		for l in range(self.weights.shape[0]):
-			acc2 += self.weights[l]**2
+		for l in range(L):
+			acc2 += sum(sum(self.theta(l, weights)[:, 1:]**2))
 
-		return (1 / m) * (acc + self.penalty * acc2 / 2)
-
-	def cost(self, X, y):
-		# y has shape m by K
-		# h has shape m by K
-		L = len(self.nodes) # number of layers (including input and output layers)
-		K = self.nodes[-1] # dimension of output layer (number of classes)
-		m = X.shape[0] # number of training examples
-		h = self.h(X) # output matrix
-
-		acc = 0
-		for i in range(1, m+1):
-			acc -= y[i]*np.log(h[i]).transpose() + (1-y[i])*np.log(1-h[i]).transpose()
-
-		return (1 / m) * (acc + self.penalty * sum(self.weights**2) / 2)
+		return (1/m) * (acc + (self.penalty/2)*acc2)
 
 	def cost_gradient(self, i):
 		# Uses backprop algorithm from Andrew Ng's ML Course.
 		# TODO
 		print('Under construction.')
+
+	def numerical_gradient(self, e, X, y):
+		numgrad = np.zeros(self.weights.shape)
+		perturb = np.zeros(self.weights.shape)
+		for i in range(self.weights.size):
+			perturb[i] = e
+			loss1 = self.cost(X, y, self.weights - perturb)
+			loss2 = self.cost(X, y, self.weights + perturb)
+			numgrad[i] = (loss1 - loss2)/(2*e)
+			perturb[i] = 0
+		return numgrad
 
 	def train(self, X):
 		# TODO
